83be53e60ccc992a13b1a8aaf2ae87284f25a599
SimIceFed <simonisefjaer@gmail.com>
2025-09-26 18:20:27 +0200

    removed-socket-server-practice-tools

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -1,6 +1,5 @@
 export const API_BASE_URL = "https://v2.api.noroff.dev";
 export const NOROFF_API_KEY = "dc0a02eb-3720-46ad-a716-b3d4621eeb77";
-export const VITE_SOCKET_URL = "http://localhost:3000";
 
 /**
  * Safely create a DOM element from an HTML template string.
